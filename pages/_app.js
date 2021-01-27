a9862990f9dba1921df00d6cac81ca2dae421fac
Brendhon <brendhon.e.c.m@gmail.com>
2021-01-26 21:05:38 -0300

    use: image

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,26 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Windlass';
     src: url('../assets/font/Windlass.ttf') format("ttf");
   }
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Windlass', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 const theme = db.theme
@@ -17,8 +33,8 @@ const theme = db.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import styled from 'styled-components'
+import img from '../assets/img/wallpaper.png'
 
-const Title = styled.h1`
-  font-size: 80px;
-  font-family: 'Windlass', sans-serif;
-  color: ${({ theme }) => theme.colors.primary};
+const Background = styled.div`
+  background-image: url(${img});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page ABC</Title>
+  return (
+    <Background>
+      <QuizContainer>
+        aaa
+      </QuizContainer>
+    </Background>
+  )
 }
